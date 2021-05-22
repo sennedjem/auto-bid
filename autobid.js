5ab11c396e5ac7320d2085ebb0be156fa0c32c01
sennedjem <sebastianariell@gmail.com>
2021-05-22 13:56:24 -0300

    Update autobid.js

--- autobid.js
+++ autobid.js
@@ -1,12 +1,12 @@
 var n = 0
 
 function falopa(){
-	var lengthItems = getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._listController._paginationViewModel._paginationList._collection.length;
+	var lengthItems = getPageItems().length;
 	if(n<lengthItems){
 		var item = obtenerSiguienteItem(n)
 		pasarASiguienteItem(item)
 		var bidParams = {_observers:[{cb:getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._itemDetailController._currentController._onBid,scope:getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._itemDetailController._currentController}]}
-		var precio = item._auction.currentBid?item._auction.currentBid:item._auction.startingBid
+		var precio = getBidPrice(item)
 		pujar(bidParams,precio)
 		n = n+1
 		setTimeout(function myFunction() {
@@ -25,4 +25,12 @@ function obtenerSiguienteItem(n){
 
 function pasarASiguienteItem(item){
 	getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._listController._eRowSelected(getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._listController._view, "rowselect", { item: item })
+}
+
+function getPageItems(){
+	return getAppMain().getRootViewController().getPresentedViewController().getCurrentViewController().getCurrentController()._listController._paginationViewModel._paginationList._collection
+}
+
+function getBidPrice(item){
+	return item._auction.currentBid?item._auction.currentBid:item._auction.startingBid
 }
